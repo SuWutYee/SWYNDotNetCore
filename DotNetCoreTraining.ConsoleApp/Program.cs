9fe79ae78155bf2ce26e003930846606cebd0eac
Su Wut Yee <suwutyeenaing@acedatasystems.com>
2024-02-05 23:11:55 +0630

    Add project files.

---
+++ DotNetCoreTraining.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+DataTable dt = new DataTable();
+string query = "select * from Tbl_Blog";
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = ".";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.IntegratedSecurity = true;
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+sqlConnection.Open();
+SqlCommand cmd = new SqlCommand(query,sqlConnection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+adapter.Fill(dt);
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine($"Title : {dr["BlogTitle"]} >>> Author : {dr["BlogAuthor"]} >>> Content : {dr["BlogContent"]}" );
+}
+sqlConnection.Close();
+
