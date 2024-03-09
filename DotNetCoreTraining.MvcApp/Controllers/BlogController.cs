8111a879fc04f312ef389c703bda5b3cf755efa9
Su Wut Yee <suwutyeenaing@acedatasystems.com>
2024-03-09 19:22:47 +0630

    - .Net Core MVC CRUD Example [Done]

--- DotNetCoreTraining.MvcApp/Controllers/BlogController.cs
+++ DotNetCoreTraining.MvcApp/Controllers/BlogController.cs
@@ -1,13 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCoreTraining.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCoreTraining.MvcApp.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly BlogDbContext _context;
+
+        public BlogController()
+        {
+            _context = new BlogDbContext();
+        }
+
         [ActionName("Index")]
         public IActionResult BlogIndex()
         {
-            return View();
+            var lstBlog = _context.Blogs.ToList();
+            return View("BlogIndex", lstBlog);
+        }
+
+        [ActionName("Edit")]
+        public IActionResult EditBlog(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return Redirect("/Blog");
+            }
+            return View("EditBlog", item);
+        }
+
+        [ActionName("Create")]
+        public IActionResult CreateBlog()
+        {
+            return View("CreateBlog");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult SaveBlog(BlogModel blog)
+        {
+            _context.Blogs.Add(blog);
+            _context.SaveChanges();
+
+            return Redirect("/Blog");
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult UpdateBlog(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return Redirect("/Blog");
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            _context.SaveChanges();
+            return Redirect("/Blog");
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if(item == null)
+            {
+                return Redirect("/Blog");
+            }
+
+            _context.Blogs.Remove(item);
+            _context.SaveChanges();
+
+            return Redirect("/Blog");
         }
     }
 }
