564a85ae498cae990a1d03c3f87c3be70d83cf86
Su Wut Yee <suwutyeenaing@acedatasystems.com>
2024-03-09 13:50:59 +0630

    - Create MVC App

---
+++ DotNetCoreTraining.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreTraining.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ DotNetCoreTraining.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCoreTraining.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
