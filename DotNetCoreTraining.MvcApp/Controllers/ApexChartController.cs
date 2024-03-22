84ee2dd50f7b5cdbea20ed60cac1b9fbd483e729
suwutyeenaing@gmail.com <suwutyeenaing@gmail.com>
2024-03-22 16:19:12 +0630

    C# Model + JS Chart [Inprogress]

--- DotNetCoreTraining.MvcApp/Controllers/ApexChartController.cs
+++ DotNetCoreTraining.MvcApp/Controllers/ApexChartController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCoreTraining.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCoreTraining.MvcApp.Controllers
 {
     public class ApexChartController : Controller
     {
         public IActionResult RadarChart()
+        {
+            ApexChartResponseModel model = new ApexChartResponseModel
+            {
+                Data = new List<int> { 20, 100, 40, 30, 50, 80, 33 },
+                Category = new List<string> { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" }
+            };
+            return View(model);
+        }
+
+        public IActionResult MixedLineColumnAreaChart()
         {
             return View();
         }
---
+++ DotNetCoreTraining.MvcApp/Models/ApexChartResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCoreTraining.MvcApp.Models
+{
+    public class ApexChartResponseModel
+    {
+        public List<int> Data { get; set; }
+        public List<string> Category { get; set; }
+    }
+}
