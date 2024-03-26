1cbbe1ed03f2648fbd4b2710599fcea124fb62b3
suwutyeenaing@gmail.com <suwutyeenaing@gmail.com>
2024-03-26 14:37:26 +0630

    - High Charts Examples [Done]

---
+++ DotNetCoreTraining.MvcApp/Controllers/HighChartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreTraining.MvcApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult DonutChart()
+        {
+            return View();
+        }
+
+        public IActionResult ColumnChart()
+        {
+            return View();
+        }
+    }
+}
