b22f8c841bab26c85c55bac85541c6b061d7e3d6
suwutyeenaing@gmail.com <suwutyeenaing@gmail.com>
2024-03-26 10:04:38 +0630

    - Chart JS Examples [Done]

---
+++ DotNetCoreTraining.MvcApp/Controllers/ChartJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreTraining.MvcApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult BarChart()
+        {
+            return View();
+        }
+
+        public IActionResult PolarAreaChart()
+        {
+            return View();
+        }
+    }
+}
